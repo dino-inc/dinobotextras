ee0190598518e173ebeabcf0453933744158ce5c
dino-inc <ethan8960@gmail.com>
2020-04-26 00:40:23 -0700

    Established the basic relationships between tables

--- cogs/stats.py
+++ cogs/stats.py
@@ -4,17 +4,42 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Boolean
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import exc
 import asyncio
-import random
 
+# sqlalchemy boilerplate
+Base = declarative_base()
+engine = create_engine('sqlite:///serverlogs.db')
+
+# Messages within a channel
 class Message(Base):
-    __tablename__ = 'channellog'
+    __tablename__="message"
+    id = Column(Integer, primary_key=True)
+    author = relationship("Member", secondary="member_message")
 
-    def __repr(self):
-        return f""
+# Channel within a server
+class Channel(Base):
+    __tablename__ = "channel"
 
+# Servers with the bot
+class ServerList(Base):
+    __tablename__="serverlist"
+
+# Members with messages
+class Member(Base):
+    __tablename__="member"
+    id = Column(Integer, primary_key=True)
+    messages = relationship("Message", secondary="member_message")
+
+# Junction between members and messages
+class Member_Message(Base):
+    __tablename__="member_message"
+    member_id = Column(Integer, ForeignKey('member.id'), primary_key = True)
+    message_id = Column(Integer, ForeignKey('message.id'), primary_key = True)
+
+Base.metadata.create_all(engine)
 
 class Stats(commands.Cog):
     def __init__(self, bot):
