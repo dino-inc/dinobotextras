09ee69fb2cc12482ec5863ba766af549e6167faa
dino-inc <ethan8960@gmail.com>
2020-04-26 19:13:45 -0700

    Added more variables to the database schema

--- cogs/stats.py
+++ cogs/stats.py
@@ -17,20 +17,37 @@ engine = create_engine('sqlite:///serverlogs.db')
 class Message(Base):
     __tablename__="message"
     id = Column(Integer, primary_key=True)
+    content = Column(String)
+    bot = Column(Boolean)
+    has_embed = Column(Boolean)
+    is_pinned = Column(Boolean)
+    date = Column(DateTime(timezone=True))
+    edited = Column(DateTime(timezone=True))
+    reactions = relationship("Reaction", backref="message")
+    attachments = relationship("Attachment", backref="message")
     author = relationship("Member", secondary="member_message")
 
 # Channel within a server
 class Channel(Base):
     __tablename__ = "channel"
+    topic = Column(String)
+    creation_date = Column(DateTime(timezone=True))
+    messages = relationship("Message", backref = "channel")
 
 # Servers with the bot
 class ServerList(Base):
     __tablename__="serverlist"
+    id = Column(Integer)
+    member_count = Column(Integer)
+    creation_date = Column(Integer)
+    channels = relationship("Channel", backref = "serverlist")
 
 # Members with messages
 class Member(Base):
     __tablename__="member"
     id = Column(Integer, primary_key=True)
+    join_date = Column(DateTime(timezone=True))
+    creation_date = Column(DateTime(timezone=True))
     messages = relationship("Message", secondary="member_message")
 
 # Junction between members and messages
@@ -38,6 +55,21 @@ class Member_Message(Base):
     __tablename__="member_message"
     member_id = Column(Integer, ForeignKey('member.id'), primary_key = True)
     message_id = Column(Integer, ForeignKey('message.id'), primary_key = True)
+
+#Store attachments of a message
+class Attachment(Base):
+    __tablename__="attachment"
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    filename = Column(String)
+
+#Store reactions of a message
+class Reaction(Base):
+    __tablename__="reaction"
+    emoji_name = Column(String)
+    emoji_id = Column(String)
+    count = Column(Integer)
+    is_custom_emoji = Column(Boolean)
 
 Base.metadata.create_all(engine)
 
