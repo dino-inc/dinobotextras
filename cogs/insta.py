496069e5e585675808720812288a90dfb3ea4c79
dino-inc <ethan8960@gmail.com>
2020-12-27 12:01:26 -0800

    Post the downloaded images to discord

--- cogs/insta.py
+++ cogs/insta.py
@@ -2,9 +2,8 @@ import discord
 from discord.ext import commands
 import instaloader
 from instaloader import Post
-import pathlib
 import re
-from datetime import datetime
+import os
 class Insta(commands.Cog):
     def __init__(self, bot):
         self.insta = instaloader.Instaloader()
@@ -14,10 +13,33 @@ class Insta(commands.Cog):
         shortcode = re.search('(https://.*)/(.*)/', message.content)
         if shortcode is None:
             return
+        directory = os.fsencode("./instagram/")
+        # Empty the image directory of old images
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            os.remove(filepath)
+
+        # Create and download the post
         post = Post.from_shortcode(self.insta.context, shortcode.group(2))
-        self.insta.download_post(post, "instagram")
+        try:
+            self.insta.download_post(post, "instagram")
+        except:
+            pass
 
+        filepath = None
+        # Delete all downloaded files that aren't the image
+        for filename in os.listdir(directory):
+            if str(filename).endswith(".jpg'") or str(filename).endswith(".png'"):
+                pass
+            else:
+                filepath = os.path.join(directory, filename)
+                os.remove(filepath)
 
+        # Send all downloaded images into chat
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            photo = discord.File(fp=filepath, filename="feonge.png")
+            await message.channel.send(file=photo)
 
 def setup(bot):
     bot.add_cog(Insta(bot))
