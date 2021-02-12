cc4b83f1937c0b0bb23be88483a925fe7f2dbba1
dino-inc <ethan8960@gmail.com>
2021-02-11 16:59:13 -0800

    Added video support

--- cogs/insta.py
+++ cogs/insta.py
@@ -7,7 +7,7 @@ import os
 import json
 class Insta(commands.Cog):
     def __init__(self, bot):
-        self.insta = instaloader.Instaloader()
+        self.insta = instaloader.Instaloader(download_video_thumbnails=False)
         insta_creds = json.load(open("./auth.json"))
         self.insta.login(insta_creds["username"], insta_creds["password"])
     @commands.Cog.listener()
@@ -33,7 +33,7 @@ class Insta(commands.Cog):
         filepath = None
         # Delete all downloaded files that aren't the image
         for filename in os.listdir(directory):
-            if str(filename).endswith(".jpg'") or str(filename).endswith(".png'"):
+            if str(filename).endswith(".jpg'") or str(filename).endswith(".png'") or str(filename).endswith(".mp4"):
                 pass
             else:
                 filepath = os.path.join(directory, filename)
@@ -42,7 +42,13 @@ class Insta(commands.Cog):
         # Send all downloaded images into chat
         for filename in os.listdir(directory):
             filepath = os.path.join(directory, filename)
-            photo = discord.File(fp=filepath, filename="feonge.png")
+            photo = None
+            if(filename).endswith(".jpg"):
+                photo = discord.File(fp=filepath, filename="feonge.jpg")
+            elif(filename).endswith(".png"):
+                photo = discord.File(fp=filepath, filename="feonge.png")
+            elif(filename.endswith(".mp4")):
+                photo = discord.File(fp=filepath, filename="feonge.mp4")
             await message.channel.send(file=photo)
 
 def setup(bot):
