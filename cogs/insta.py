10c9324df9e7ab3e6f72586c5965062ff79721d8
dino-inc <ethan8960@gmail.com>
2021-02-11 17:18:37 -0800

    Added error output to discord upon failure

--- cogs/insta.py
+++ cogs/insta.py
@@ -27,10 +27,12 @@ class Insta(commands.Cog):
                 return
             post = Post.from_shortcode(self.insta.context, shortcode.group(2))
             self.insta.download_post(post, "instagram")
-        except:
+        except Exception as e: 
+            message.channel.send(f"Unable to download post; error is {e}") 
             pass
 
         filepath = None
+        try:
         # Delete all downloaded files that aren't the image
         for filename in os.listdir(directory):
             if str(filename).endswith(".jpg'") or str(filename).endswith(".png'") or str(filename).endswith(".mp4"):
@@ -38,19 +40,26 @@ class Insta(commands.Cog):
             else:
                 filepath = os.path.join(directory, filename)
                 os.remove(filepath)
+        except Exception as e:
+            message.channel.send(f"Unable  to clean the directory after downloading images; error is {e}")
+            pass
 
+        try:
         # Send all downloaded images into chat
         for filename in os.listdir(directory):
             filepath = os.path.join(directory, filename)
             photo = None
-            if(filename).endswith(".jpg"):
+            print(filename)
+            if(str(filename).endswith(".jpg")):
                 photo = discord.File(fp=filepath, filename="feonge.jpg")
-            elif(filename).endswith(".png"):
+            elif(str(filename).endswith(".png")):
                 photo = discord.File(fp=filepath, filename="feonge.png")
-            elif(filename.endswith(".mp4")):
+            elif(str(filename).endswith(".mp4")):
                 photo = discord.File(fp=filepath, filename="feonge.mp4")
             await message.channel.send(file=photo)
-
+        except Exception as e:
+            message.channel.send(f"Unable to send media; error is {e}")
+            return
 def setup(bot):
     bot.add_cog(Insta(bot))
 
