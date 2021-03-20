01ff665f5ef2167f937aeeccb4442977564f53b3
dino-inc <ethan8960@gmail.com>
2021-03-19 22:20:53 -0700

    Added deviantart ripper

--- cogs/insta.py
+++ cogs/insta.py
@@ -5,6 +5,10 @@ from instaloader import Post
 import re
 import os
 import json
+import requests
+from lxml import html
+import io
+
 class Insta(commands.Cog):
     def __init__(self, bot):
         self.insta = instaloader.Instaloader(download_video_thumbnails=False)
@@ -21,17 +25,16 @@ class Insta(commands.Cog):
         for filename in os.listdir(directory):
             filepath = os.path.join(directory, filename)
             os.remove(filepath)
-        if "instagram" not in shortcode.group(1):
+        if "instagram" not in shortcode.group(1) and "deviantart" not in shortcode.group(1):
+            print("no valid site")
             return
         # Create and download the post
         async with message.channel.typing():
-            try:
-                post = Post.from_shortcode(self.insta.context, shortcode.group(2))
-                self.insta.download_post(post, "instagram")
-            except Exception as e:
-                await message.channel.send(f"Unable to download post; error is {e}")
+            if "instagram" in shortcode.group(1):
+                await instagram_rip(self, shortcode, message)
+            elif "deviantart" in shortcode.group(1):
+                await deviantart_rip(self, message)
                 return
-
             filepath = None
             try:
                 # Delete all downloaded files that aren't the image
@@ -56,6 +59,33 @@ class Insta(commands.Cog):
                 except Exception as e:
                     await message.channel.send(f"Unable to send media; error is {e}")
                     return
+
+async def instagram_rip(self, shortcode, message):
+    try:
+        post = Post.from_shortcode(self.insta.context, shortcode.group(2))
+        self.insta.download_post(post, "instagram")
+        return True
+    except Exception as e:
+        await message.channel.send(f"Unable to download instagram post; error is {e}")
+        return False
+
+async def deviantart_rip(self, message):
+    try:
+        page = requests.get(message.content)
+        raw_html = html.fromstring(page.content)
+        image = raw_html.xpath('//*[@id="root"]/main/div/div[1]/div[1]/div/div[2]/div[1]/div/img/@src')
+        title = raw_html.xpath('//*[@id="root"]/main/div/div[1]/div[1]/div/div[2]/div[1]/div/img/@alt')
+        #compile image from chunks
+        image_request = requests.get(image[0], stream=True).raw.data
+        raw_image = io.BytesIO(image_request)
+        discord_file = discord.File(fp=raw_image, filename=title[0]+'.png')
+        await message.channel.send(file=discord_file)
+        print(f"Successfully posted image {title[0]}.")
+        return True
+    except Exception as e:
+        await message.channel.send(f"Unable to download deviantart post; error is {e}")
+        return False
+
 def setup(bot):
     bot.add_cog(Insta(bot))
 
